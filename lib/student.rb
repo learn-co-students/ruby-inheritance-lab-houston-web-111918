708595b4390cdf8e9884f112b720adcc88d6276a
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-26 16:32:17 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge, :learn
+    def initialize (knowledge=[])
+        @knowledge = knowledge
+
+    end
+
+    def learn (knowledge)
+        @knowledge << knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
